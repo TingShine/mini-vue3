0bd673b2366ab029d5280af3a70c11ce8c4e8d30
shine <791405318@qq.com>
2022-10-11 10:03:20 +0800

    ci: 修改打包前缀

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
+  base: "/mini-vue3/",
   lang: "zh-CN",
   title: "Mini-Vue3",
   description: "学习vue3源码，再造框架",
