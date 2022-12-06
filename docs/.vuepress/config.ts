a60117f3bd4f629f0cdd2bfc9e7c551eb2495b6c
shine <791405318@qq.com>
2022-12-06 20:33:14 +0800

    ci: update setup-node version

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,15 @@
 import { defaultTheme, defineUserConfig } from "vuepress";
+import { backToTopPlugin } from "@vuepress/plugin-back-to-top";
+import { searchPlugin } from "@vuepress/plugin-search";
 
 export default defineUserConfig({
   base: "/mini-vue3/",
+  plugins: [backToTopPlugin(), searchPlugin()],
   lang: "zh-CN",
   title: "Mini-Vue3",
   description: "学习vue3源码，再造框架",
   theme: defaultTheme({
+    repo: "https://github.com/TingShine/mini-vue3.git",
     sidebar: [
       {
         text: "介绍",
