4773d90ff8a5036e4a0aee61557552bcf1d27b52
shine <791405318@qq.com>
2022-12-22 00:12:54 +0800

    perf: 优化reactive代码风格

--- packages/reactive/basehandlers.ts
+++ packages/reactive/basehandlers.ts
@@ -1,31 +1,87 @@
+import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { ReactiveFlags } from "./reactive";
+import { reactive, ReactiveFlags, reactiveMap, readonly, readonlyMap, shallowReadonlyMap } from "./reactive";
 
-const get = (target, key, reciever) => {
-  // 获取原对象
-  if (key === ReactiveFlags.RAW) {
-    return target
+export const createGetter = (isReadonly = false, shallowReadonly = false) => {
+  return (target, key, receiver) => {
+    const isExistReactiveMap = () => key === ReactiveFlags.RAW && reactiveMap.get(target) === receiver
+    const isExistReadonlyMap = () => key === ReactiveFlags.RAW && readonlyMap.get(target) === receiver
+    const isExistShallowReadonlyMap = () => key === ReactiveFlags.RAW && shallowReadonlyMap.get(target) === receiver
+
+    // 获取原对象
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly
+    } else if (
+      isExistReactiveMap() ||
+      isExistReadonlyMap() ||
+      isExistShallowReadonlyMap()
+    ) {
+      return target
+    }
+
+    const result = Reflect.get(target, key, receiver);
+
+    if (!isReadonly) {
+      track(target, "get", key);
+    }
+
+    if (shallowReadonly) {
+      return result
+    }
+
+    if (isObject(result)) {
+      return isReadonly ? readonly(result) : reactive(result)
+    }
+
+    return result
   }
+}
 
-  // 获取是否为reactive对象
-  if (key === ReactiveFlags.IS_REACTIVE) {
-    return true
+export const createSetter = () => {
+  return (target, key, value, receiver) => {
+    const result = Reflect.set(target, key, value, receiver)
+  
+    // 触发依赖
+    trigger(target, key);
+  
+    return result
   }
-
-  track(target, "get", key);
-
-  return Reflect.get(target, key, reciever);
 }
-const set = (target, key, value, reciever) => {
-  const result = Reflect.set(target, key, value, reciever)
 
-  // 触发依赖
-  trigger(target, key);
-
-  return result
-}
+// reavtive
+const get = createGetter()
+const set = createSetter()
 
 export const mutableHandlers = {
   get,
   set
+}
+
+// readonly
+const readonlyGet = createGetter(true)
+const readonlySet = (target, key) => {
+  // readonly的对象不允许赋值
+  console.warn(`Failed on key ${key}: target is readonly.`, target)
+
+  return true
+}
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set: readonlySet
+}
+
+// shallow
+const shallowGet = createGetter(false, true)
+const shallowReadonlySet = (target, key) => {
+  console.warn(`Failed on key ${key}: target is readonly.`, target)
+
+  return true
+}
+
+export const shallowReadonlyHandlers = {
+  get: shallowGet,
+  set: shallowReadonlySet
 }
--- packages/reactive/reactive.ts
+++ packages/reactive/reactive.ts
@@ -1,14 +1,25 @@
-import { mutableHandlers } from "./basehandlers";
+import { mutableHandlers, readonlyHandlers, shallowReadonlyHandlers } from "./basehandlers";
 
-const targetMap = new WeakMap()
+export const reactiveMap = new WeakMap()
+export const readonlyMap = new WeakMap()
+export const shallowReadonlyMap = new WeakMap()
 
 export enum ReactiveFlags {
   RAW = '_v_raw_',
-  IS_REACTIVE = '_v_isReactive'
+  IS_REACTIVE = '_v_isReactive',
+  IS_READONLY = '_v_isReadonly'
 }
 
 export function reactive (target) {
-  return createReactiveObject(target, targetMap, mutableHandlers);
+  return createReactiveObject(target, reactiveMap, mutableHandlers)
+}
+
+export function readonly (target) {
+  return createReactiveObject(target, readonlyMap, readonlyHandlers)
+}
+
+export function shallowReadonly (target) {
+  return createReactiveObject(target, shallowReadonlyMap, shallowReadonlyHandlers)
 }
 
 export function createReactiveObject (target, proxyMap, baseHandlers) {
